7f3eec26aae30958ba4591186aba700801928eee
Priscila <prisci_sg@hotmail.com>
2017-03-04 13:39:22 -0700

    init commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php $page = 'index'; ?>
+<?php include('header.php'); ?>
+
+
+
+<div class="outer background">
+    <div class="align">
+        <text style="font-weight:600">Username</text> <input type="text" name="user" class=""><br>
+        <text style="font-weight:600">NIP</text> <input type="password" name="nip" class=""><br>
+        <div style="text-align:center">
+            <a href ="menu.php"><input type="submit" value="Submit" style="margin:10px"></a>
+        </div>
+    </div>
+
+</div>
+
+
+
+
+<?php include('footer.php'); ?>
---
+++ menu.php
@@ -0,0 +1,22 @@
+<?php $page = 'menu'; ?>
+<?php include('header.php'); ?>
+
+
+
+<div class="outer background">
+    <div class="row">
+        <div class="col-sm-4">
+            <div id="itinerary">Itinerary</div>
+        </div>
+        <div class="col-sm-4">
+            <div id="creditcards">Credit/Debit Cards</div>
+        </div>
+        <div class="col-sm-4">
+            <div id="documents">Documents</div>
+        </div>
+    </div>
+</div>
+
+
+
+    <?php include('footer.php'); ?>
